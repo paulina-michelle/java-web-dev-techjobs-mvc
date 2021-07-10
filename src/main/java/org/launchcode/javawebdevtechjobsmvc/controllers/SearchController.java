5238e6315c5e18265da2066b2a39e23ad51914ab
Paulina <paulinah09@gmail.com>
2021-07-09 19:55:34 -0500

    updated displaySearchResults handler method (changed if statement to recognize searchTerm instead of searchType) + added bullet formatting to "view all" in list

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -28,7 +28,7 @@ public class SearchController {
     @RequestMapping(value = "results")
     public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
         ArrayList<Job> jobs;
-        if ((searchType.toLowerCase().equals("all"))||searchType.toLowerCase().equals("")){
+        if ((searchTerm.toLowerCase().equals("all"))||searchTerm.toLowerCase().equals("")){
             jobs = JobData.findAll();
             model.addAttribute("title", "All Jobs");
         } else {
